fb3e209b04114bfabc95378636c6767b0c98cdc4
ivan <ivan.tong97@gmail.com>
2020-02-07 17:01:21 -0500

    Fitness All Done

--- app/src/main/java/com/chentong/keepapp/ui/fitness/tabs/ExerciseFragment.java
+++ app/src/main/java/com/chentong/keepapp/ui/fitness/tabs/ExerciseFragment.java
@@ -1,14 +1,8 @@
 package com.chentong.keepapp.ui.fitness.tabs;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,11 +11,9 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.chentong.keepapp.R;
-import com.chentong.keepapp.ui.fitness.tabs.details.SportFragment;
 
 
 public class ExerciseFragment extends Fragment {
-
 
 
     @Override
